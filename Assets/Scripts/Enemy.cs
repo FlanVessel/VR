f1bd1682ba09df3f26cf8c36400a77e7cbd1562b
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-09-10 15:53:26 -0600

    Tenemos un EnemyManager y Enemy (no he resuelto la animacion)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isActive;
+    public int vida = 5;
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int EnemyCount;
+    public List<GameObject> spawners;
+    private void Start()
+    {
+        SpawnEnemies();
+    }
+    void SpawnEnemies()
+    { 
+        for(int i = 0; i < EnemyCount; i++) 
+        { 
+            GameObject ins = Instantiate(Prefab, spawners[Random.Range (0,spawners.Count)].transform.position, Quaternion.identity);
+            ins.GetComponent<NavMeshAgent>().SetDestination(GameObject.FindWithTag("Player").transform.position);
+        }
+    }  
+}
